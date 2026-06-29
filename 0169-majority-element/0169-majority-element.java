568db46858d0f0a439497d4f6329c447bb4fbe8f
sumit shinde <sumit.shinde_24uds@sanjivani.edu.in>
2026-06-29 19:52:13 +0530

    Time: 7 ms (44%), Space: 55.8 MB (31.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,25 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         int major=nums[0];
+
+//         for(int i=0;i<n-1;i++)
+//         {
+//             int count = 0;
+//             for (int j = 0; j < n; j++) {
+//                 if (nums[j] == nums[i]) {
+//                     count++;
+//                 }
+//             }
+//             if(count >(n/2))
+//             {
+//                 major=nums[i];
+//                 break;
+//             }
+//         }return major;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
       int n=nums.length;
