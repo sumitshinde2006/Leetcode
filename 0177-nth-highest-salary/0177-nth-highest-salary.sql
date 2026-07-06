d0366d8d6628e911c0d4547a8b6fda980329dc7b
sumit shinde <sumit.shinde_24uds@sanjivani.edu.in>
2026-07-06 07:36:56 +0530

    Time: 426 ms (94.74%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+    select distinct salary from Employee 
+    order by salary desc
+    limit 1 offset M
+  );
+END
